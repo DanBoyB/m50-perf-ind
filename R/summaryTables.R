4cce0bf67947e8d46b7e5b612c33c54b244a14cf
DanB <dannyboybrennan@gmail.com>
2017-03-31 16:41:39 +0100

    draft summary tables

---
+++ R/summaryTables.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(purrr)
+library(lubridate)
+
+vkm <- readRDS("output/vkm/vkm2015.rds") %>% 
+    group_by(sectionName, month, period, direction) %>% 
+    summarise(vkm = sum(vkm),
+              mvkm = sum(mvkm))
+
+stableFlow <- readRDS("output/stableFlow/stableFlow.rds") %>% 
+    group_by(sectionName, month, period, direction) %>% 
+    summarise(stableHours = sum(stableHours),
+              monthlyHours = sum(monthlyHours)) %>% 
+    mutate(percStable = stableHours / monthlyHours)
+
+bufferMisery <- readRDS("output/bufferMisery/bufferMisery.rds") %>% 
+    filter(sectionName == "N2 to Ballymun")
+    
+    group_by(sectionName, month, period, direction) %>% 
+    summarise(buffTimeIndex = weighted.mean(buffTimeIndex, vkt),
+              miseryIndex = weighted.mean(miseryIndex, vkt))
+
+combStats <- vkm %>% 
+    bind_cols(stableFlow[7]) %>% 
+    bind_cols(bufferMisery[5:6])
