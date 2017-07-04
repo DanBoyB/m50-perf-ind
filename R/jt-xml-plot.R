af503bb2edc8ae80dfe19ba79ce37a44d86db772
DanB <dannyboybrennan@gmail.com>
2017-07-04 12:18:55 +0100

    updated plot

--- R/jt-xml-plot.R
+++ R/jt-xml-plot.R
@@ -12,22 +12,22 @@ a <- read_csv("data/jt/all.csv", col_names = c("datetime",
     arrange(datetime)
 
 a %>% 
-    mutate(hour = hour(datetime),
+    mutate(hour = (datetime),
            date = date(datetime),
-           time = format(datetime, "%H:%M")) %>%
+           day = wday(datetime, label = TRUE),
+           time = format(as.POSIXct(strptime(datetime, "%Y-%m-%d  %H:%M:%S",tz="")) ,
+                         format = "%H:%M"),
+           time = as.POSIXct(time, format = "%H:%M")) %>%
     filter(site_ref == "ROI_ANPR_M50S.32-N11S.1270") %>% 
     ggplot(aes(x = time)) +
-    geom_line(aes(y = trav_time / 60, group = date), alpha = 0.3) +           
+    geom_point(aes(y = trav_time / 60, group = date, colour = day), alpha = 0.1) + 
+    facet_wrap(~ day, ncol = 1) +
     #geom_line(aes(y = ff_trav_time / 60), colour = "red", size = 2) +
     coord_cartesian(ylim = c(0, 150)) +
-    scale_x_discrete() +
-    theme_minimal()
-    
-a %>% 
-    mutate(time = format(datetime, "%H:%M"),
-           na_flag = ifelse(is.na(trav_time), hour(datetime), NA)) %>% 
-    filter(site_ref == "ROI_ANPR_M50S.32-N11S.1270") %>% 
-    ggplot(aes(na_flag)) + 
-    geom_histogram()
-
-ggplotly()
+    scale_x_datetime(labels = date_format("%H:%M"), breaks = date_breaks("2 hour")) +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    ggtitle("M50 Southbound Travel Times",
+            subtitle = "Data taken from TII opendata xml files (Aug '16 - Jan '17)") +
+    xlab("") +
+    ylab("Travel Time (mins)")
